067ad4e0413b60051ab8ddb63a54627e4c18a95f
ypron <ypron@LAPTOP-5H1KDVCM>
2025-03-25 21:33:18 +0300

    Task done

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,38 +4,62 @@ using namespace std;
 void FillRand(int arr[], const int n);// функция заполняет массив случайными числами
 void Print(int arr[], const int n); //функция вывода на экран
 void Sort(int arr[], const int n); //функция сортировки массива
-int SumNumbers(int arr[], const int n);
-double AvgNumbers(int arr[], const int n, int Sum);
-int MaxNumbers(int arr[], const int n);
-int MinNumbers(int arr[], const int n);
-void ShiftLeft(int arr[], const int n, int shift);
-void ShiftRight(int arr[], const int n, int shift);
+int SumNumbers(int arr[], const int n);//функция вычисления суммы элементов массива
+double AvgNumbers(int arr[], const int n, int Sum);// функция вычисления средн.арифметич. элементов
+int MaxNumbers(int arr[], const int n);// функция нахождения max из элементов
+int MinNumbers(int arr[], const int n);// функция нахождения min из элементов
+void ShiftLeft(int arr[], const int n, int shift);//функция смещения массива влево
+void ShiftRight(int arr[], const int n, int shift);//функция смещения массива вправо
 
 
 void main() {
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n];
-	int shift = 3;
+	int step_1 = 3;
 
 	FillRand(arr,n);
 	Print(arr, n);
 	Sort(arr, n);
+
+	cout << "Отсортированный массив: " << endl;
 	Print(arr, n);
-	ShiftLeft(arr, n, shift);
+	cout << "Сумма элементов массива: " << SumNumbers(arr, n) << endl;
+	cout << "Средняя арифметическа элементов: " << AvgNumbers(arr, n, SumNumbers(arr, n)) << endl;
+	cout << "Максимальный элемент массива: " << MaxNumbers(arr, n) << endl;
+	cout << "Минимальный элемент массива: " << MinNumbers(arr, n) << endl;
+
+	cout << "Смещение элементов массива влево: " << endl;
+	ShiftLeft(arr, n, step_1);
 	Print(arr, n);
-	ShiftRight(arr, n, shift);
+
+	cout << "Смещение элементов массива вправо: " << endl;
+	ShiftRight(arr, n, step_1);
 	Print(arr, n);
+	cout << endl << endl;;
 
 	const int SIZE = 8;
 	int brr[SIZE];
+	int step_2 = 1;
 
 	FillRand(brr, SIZE);
 	Print(brr, SIZE);
 	Sort(brr, SIZE);
+	cout << "Отсортированный массив: " << endl;
 	Print(brr, SIZE);
 	
-	
+	cout << "Сумма элементов массива: " << SumNumbers(brr, SIZE) << endl;
+	cout << "Средняя арифметическа элементов: " << AvgNumbers(brr, SIZE, SumNumbers(brr, SIZE)) << endl;
+	cout << "Максимальный элемент массива: " << MaxNumbers(brr, SIZE) << endl;
+	cout << "Минимальный элемент массива: " << MinNumbers(brr, SIZE) << endl;
+
+	cout << "Смещение элементов массива влево: " << endl;
+	ShiftLeft(brr, SIZE, step_2);
+	Print(brr, SIZE);
+
+	cout << "Смещение элементов массива вправо: " << endl;
+	ShiftRight(brr, SIZE, step_2);
+	Print(brr, SIZE);
 }
 
 void FillRand(int arr[], const int n)
@@ -73,34 +97,31 @@ int SumNumbers(int arr[], const int n)
 }
 double AvgNumbers(int arr[], const int n, int Sum)
 {
-	return Sum /= n;
+	return  (double)Sum/n;
 }
 int MaxNumbers(int arr[], const int n)
-{
-	int min_number = INT_MIN;
-	int max_number;
+{ 
+	int max_number = INT_MIN;
 	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] > min_number)  max_number = arr[i]; 
+		if (arr[i] > max_number)  max_number = arr[i];
 		
 	}
 	return max_number;
 }
 int MinNumbers(int arr[], const int n)
 {
-	int max_number = INT_MAX;
-	int min_number;
+	
+	int min_number = INT_MAX;
 	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] < max_number) min_number = arr[i];
+		if (arr[i] < min_number) min_number = arr[i];
 	}
 	return min_number;
 }
-void PrintResult()
-{ }
 void ShiftLeft(int arr[], const int n, int shift)
-
 {
+	if (shift < 0) shift += n;
 	for (int i = 0; i < shift; i++) {
 		int temp = arr[0];
 		for (int j = 0; j < n - 1; j++) {
@@ -111,12 +132,13 @@ void ShiftLeft(int arr[], const int n, int shift)
 }
 void ShiftRight(int arr[], const int n, int shift)
 {
+	if (shift < 0) shift += n;
 	for (int i = 0; i < shift; i++) {
-		int temp = arr[0];
-		for (int j = 0; j < n + 1; j++) {
-			arr[j] = arr[j + 1];
+		int temp = arr[n-1];
+		for (int j = n - 1; j < 0; j--) {
+			arr[j] = arr[j - 1];
 		}
-		arr[n-1] = temp;
+		arr[0] = temp;
 }
 }
 
